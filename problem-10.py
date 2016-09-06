e9438d72c38ca36b13ab553e8cacba0f006041ab
Nguyen Ngoc Hiep <hiepnn@Nguyens-MacBook-Air.local>
2016-09-06 10:50:45 +0700

    problem 10

---
+++ problem-10.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+Find the sum of all the primes below 2.000.000.
+'''
+
+
+def sum_primes(limit):
+    primes = []
+    for n in range(2, limit+1):
+        for p in primes:
+            if n % p == 0:
+                break             # if p divides n, stop the search
+            if n < p*p:
+                primes.append(n)  # if p > sqrt(n), mark n as prime and stop
+                break
+        else:
+            primes.append(n)  # fallback: only get here for n == 2
+
+    # calculator sum primes
+    sum = 0
+    for i in primes:
+        sum += i
+
+    return sum
+
+print(sum_primes(2000000))
